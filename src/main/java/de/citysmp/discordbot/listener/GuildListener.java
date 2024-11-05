526ea3ba415e17c0d07f27e079d4c92497eaf552
ezTxmMC <eztxmmc@hotmail.com>
2024-11-05 08:12:05 +0100

    added adding of role to member on guild join

--- src/main/java/de/citysmp/discordbot/listener/GuildListener.java
+++ src/main/java/de/citysmp/discordbot/listener/GuildListener.java
@@ -1,6 +1,7 @@
 package de.citysmp.discordbot.listener;
 
 import de.citysmp.discordbot.DiscordBot;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,6 +11,9 @@ public class GuildListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        Member member = event.getMember();
         Role guest = event.getGuild().getRoleById(DiscordBot.getConfig().get("Roles").asJsonObject().get("Guest").asString());
+        assert guest != null;
+        event.getGuild().addRoleToMember(member, guest).queue();
     }
 }
