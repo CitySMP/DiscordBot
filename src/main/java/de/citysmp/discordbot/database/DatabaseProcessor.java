c7e0b99a43cab54da3407ba5e61abc6d03cabe1f
ezTxmMC <eztxmmc@hotmail.com>
2024-11-05 14:15:21 +0100

    added select and delete methods to DatabaseProcessor

--- src/main/java/de/citysmp/discordbot/database/DatabaseProcessor.java
+++ src/main/java/de/citysmp/discordbot/database/DatabaseProcessor.java
@@ -20,15 +20,15 @@ public record DatabaseProcessor(SQLConnection connection) {
         this.connection.putAsync("UPDATE ? SET ?=?", table, key, value).join();
     }
 
-    public void update(String table, String key, String value, String condition) {
-        this.connection.putAsync("UPDATE ? SET ?=? WHERE ?", table, key, value, condition).join();
+    public void update(String table, String key, String value, String conditions) {
+        this.connection.putAsync("UPDATE ? SET ?=? WHERE ?", table, key, value, conditions).join();
     }
 
     public void update(String table, String keysEqualsValues) {
         this.connection.putAsync("UPDATE ? SET ?", table, keysEqualsValues).join();
     }
 
-    public void update(String table, String condition, String... keysEqualsValues) {
+    public void update(String table, String conditions, String... keysEqualsValues) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < keysEqualsValues.length; i++) {
             if (i == keysEqualsValues.length - 1) {
@@ -37,6 +37,18 @@ public record DatabaseProcessor(SQLConnection connection) {
             }
             builder.append(keysEqualsValues[i]).append(",");
         }
-        this.connection.putAsync("UPDATE ? SET ? WHERE ?", table, builder.toString(), condition).join();
+        this.connection.putAsync("UPDATE ? SET ? WHERE ?", table, builder.toString(), conditions).join();
+    }
+
+    public void select(String table, String columns) {
+        this.connection.putAsync("SELECT ? FROM ?", columns, table).join();
+    }
+
+    public void select(String table, String columns, String conditions) {
+        this.connection.putAsync("SELECT ? FROM ? WHERE ?", columns, table, conditions).join();
+    }
+
+    public void delete(String table, String conditions) {
+        this.connection.putAsync("DELETE FROM ? WHERE ?", table, conditions).join();
     }
 }
